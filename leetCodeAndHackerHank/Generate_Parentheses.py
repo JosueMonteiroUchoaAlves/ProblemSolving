9a870fc17f0cd5a8fbb73bdbde29ef02ab8b667b
Josué Monteiro <josue.player01@gmail.com>
2025-09-03 09:58:04 -0300

    Adds solution for Generate Parentheses problem
    Implements a backtracking algorithm to generate all combinations of well-formed parentheses for a given `n`. The recursive approach explores adding open and close parentheses while maintaining validity constraints, ensuring that open parentheses count does not exceed `n` and closed parentheses count does not exceed open parentheses count.

---
+++ leetCodeAndHackerHank/Generate_Parentheses.py
@@ -0,0 +1,30 @@
+# https://leetcode.com/problems/generate-parentheses/submissions/1758199050/
+from typing import List
+
+class Solution:
+    def generateParenthesis(self, n: int) -> List[str]:
+        parenthesesStack = []
+        answer = []
+        aberturas, fechamentos = 0, 0
+        def backtraking( parenthesesStack: list, aberturas: int, fechamentos: int, answer:list):
+            if (aberturas == fechamentos) and aberturas == n:
+                answer.append("".join(parenthesesStack.copy()))
+                return answer
+
+            if aberturas < n:
+                parenthesesStack.append("(")
+                aberturas += 1
+                backtraking(parenthesesStack, aberturas, fechamentos, answer)
+                parenthesesStack.pop()
+                aberturas -= 1
+            if aberturas > fechamentos:
+                parenthesesStack.append(")")
+                fechamentos += 1
+                backtraking(parenthesesStack, aberturas, fechamentos, answer)
+                parenthesesStack.pop()
+                fechamentos -= 1
+        backtraking(parenthesesStack, aberturas, fechamentos, answer)
+        return answer
+
+S = Solution()
+print(S.generateParenthesis(3))
