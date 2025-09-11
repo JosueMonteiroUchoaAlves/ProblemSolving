4179d9ed65392b5e507b059cb914d68aa2cc58a8
Josué Monteiro <josue.player01@gmail.com>
2025-09-11 09:16:29 -0300

    Improves string interleaving logic
    Refactors the string combination algorithm to correctly handle input strings of varying lengths. The previous approach could lead to incorrect or incomplete string generation when one string was longer than the other. The new implementation explicitly appends remaining characters from the longer string, ensuring a complete and accurate interleaved output.

--- BeeCrowd/combinador.py
+++ BeeCrowd/combinador.py
@@ -1,10 +1,15 @@
+# https://judge.beecrowd.com/pt/runs/code/46532822
 N = int(input())
-j = ""
 for _ in range(N):
-  s = input().split()
-
-  for i in range(len(s[0])):
-    j = j+s[0][i]+s[1][i]
-  j = j+s[1][i+1:]
-  print(j)
-  j = ""
+    ans = ""
+    strings = input().split()
+    primeiraString = strings[0]
+    SegundaString = strings[1]
+    for i in range(len(primeiraString)):
+        if len(SegundaString) <= i:
+            ans = ans + primeiraString[i:]
+            break
+        ans = ans + primeiraString[i] + SegundaString[i]
+    else:
+        ans = ans + SegundaString[i+1:]
+    print(ans)
