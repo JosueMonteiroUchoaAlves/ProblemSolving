c3c812c06313d87f84cd397851b4cf3f8fdb4c93
Josué Monteiro <josue.player01@gmail.com>
2025-09-11 09:21:58 -0300

    Optimizes string combination performance
    Refactors string building from direct concatenation in a loop to appending parts to a list. This improves performance by avoiding the creation of numerous intermediate string objects, utilizing `"".join()` for efficient final string construction.

--- BeeCrowd/combinador.py
+++ BeeCrowd/combinador.py
@@ -1,15 +1,16 @@
 # https://judge.beecrowd.com/pt/runs/code/46532822
 N = int(input())
 for _ in range(N):
-    ans = ""
+    ans = []
     strings = input().split()
     primeiraString = strings[0]
     SegundaString = strings[1]
     for i in range(len(primeiraString)):
         if len(SegundaString) <= i:
-            ans = ans + primeiraString[i:]
-            break
-        ans = ans + primeiraString[i] + SegundaString[i]
+            ans.append(primeiraString[i:])
+            break   
+        ans.append(primeiraString[i])
+        ans.append(SegundaString[i])
     else:
-        ans = ans + SegundaString[i+1:]
-    print(ans)
+        ans.append(SegundaString[i+1:])
+    print("".join(ans))
