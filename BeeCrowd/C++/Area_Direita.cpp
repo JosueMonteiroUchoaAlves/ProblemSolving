e31e25b6b2e227ff8c2f6e034cc206b279ecdbe8
Josue Monteiro <jmua@aluno.ifnmg.edu.br>
2026-04-06 18:44:43 -0300

    Adds solution: for judge.beecrowd 1190 ( Área Direita)

---
+++ BeeCrowd/C++/Area_Direita.cpp
@@ -0,0 +1,47 @@
+#include <bits/stdc++.h>
+// 1190 - Área Direita
+// https://judge.beecrowd.com/pt/problems/view/1187
+
+using namespace std;
+
+int main(){
+    char op=' ';
+    cin >> op;
+    int counter=0;
+    double elem=0, elemSum=0;
+
+    for(int i=0;i<12;i++){
+        for(int j=0;j<12;j++){
+            cin >> elem;
+
+            if (j > 6){
+                if ((j > i) && (i + j + 2 > 12 + 1)){
+                    counter++;
+                    elemSum += elem;
+                }
+            }
+        }
+    }
+    if (op=='S'){
+        cout << fixed << setprecision(1) << elemSum << endl;
+    }
+    else{
+        cout << fixed << setprecision(1) << elemSum/counter << endl;
+    }
+
+    return 0;
+}
+/*
+1 1 1 1 1 1 1 1 1 1 1 1
+1 1 1 1 1 1 1 1 1 1 1 2
+1 1 1 1 1 1 1 1 1 1 2 2
+1 1 1 1 1 1 1 1 1 2 2 2
+1 1 1 1 1 1 1 1 2 2 2 2
+1 1 1 1 1 1 1 2 2 2 2 2
+1 1 1 1 1 1 1 2 2 2 2 2
+1 1 1 1 1 1 1 1 2 2 2 2
+1 1 1 1 1 1 1 1 1 2 2 2
+1 1 1 1 1 1 1 1 1 1 2 2
+1 1 1 1 1 1 1 1 1 1 1 2
+1 1 1 1 1 1 1 1 1 1 1 1
+*/
