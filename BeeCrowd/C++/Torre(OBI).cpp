eba3113cb19412a34f7121451d9ca2236221195a
Josue Monteiro <jmua@aluno.ifnmg.edu.br>
2026-04-07 16:15:19 -0300

    Refactoring solution: for neps.academy 45 (Tower - OBI 2015)
    An nice and more efficient solution to avoid thousands of "IF" calls, resulting in an O(3*N²) solution.

--- BeeCrowd/C++/Torre(OBI).cpp
+++ BeeCrowd/C++/Torre(OBI).cpp
@@ -4,20 +4,16 @@
 
 using namespace std;
 
-int SomarLinha(vector<int> linha){
-    return accumulate(linha.begin(), linha.end(), 0);
-}
-
 int main(){
     int n, maiorCruzamentoAtual=0;
     cin >> n;
 
-    vector<int> sumLinha(n, -1); //  Elemento I corresponde a linha I
+    vector<int> sumLinha(n, 0); //  Elemento I corresponde a linha I
     /* 
     -1 porque a soma da linha/coluna pode ser 0 e se for nao quero que 
     meu IF me faça somar aquela linha denovo
     */
-    vector<int> sumColuna(n, -1); //  Elemento J corresponde a linha J
+    vector<int> sumColuna(n, 0); //  Elemento J corresponde a linha J
 
     vector<vector<int>> matriz(n, vector<int>(n, 0));
 
@@ -26,18 +22,22 @@ int main(){
             cin >> matriz[i][j];
         }
     }
+    /*
+    Ao invés de me perguntar toda vez se 
+    aquela linha ou coluna foi calculada,vou calcular todas logo!
+    Pois independente de qualquer coisa essa "passada" pela matriz para
+    fazer o calculo teria que acontecer. Vai sair mais barato do que aqueles
+    IFs se repetindo 1milhao de vezes.
+    */ 
+    for (int i = 0; i < n; i++){
+        for (int j = 0; j < n; j++){
+            sumLinha[i] += matriz [i][j];
+            sumColuna[j] += matriz [i][j]; 
+        }
+    }
 
     for (int i = 0; i < n; i++){
         for (int j = 0; j < n; j++){
-            if (sumLinha[i] == -1){
-                sumLinha[i] = SomarLinha(matriz[i]);
-            }
-            if (sumColuna[j] == -1){
-                sumColuna[j] += 1;
-                for (int i=0; i<n; i++){
-                    sumColuna[j] += matriz[i][j];
-                }
-            }
             if (sumLinha[i] + sumColuna[j] - (matriz[i][j] * 2) > maiorCruzamentoAtual){
                 maiorCruzamentoAtual = sumLinha[i] + sumColuna[j] - (matriz[i][j] * 2);
             }
