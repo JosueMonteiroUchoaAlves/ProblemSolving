d5a029717f680cc7c9ff0d7f389520c1ae3d93a6
Josue Monteiro <jmua@aluno.ifnmg.edu.br>
2026-06-18 18:30:33 -0300

    Refactoring solution: for judge.beecrowd 1025 (Where is the marble? - SEU Bangladesh)
    I gained a better understanding of the lower and upper bound functions and used them more efficiently, resulting in a more effective solution.
    
    Turned off the IO sync

--- BeeCrowd/C++/Where_Is_The_Marble.cpp
+++ BeeCrowd/C++/Where_Is_The_Marble.cpp
@@ -3,25 +3,24 @@
 using namespace std;
 
 int main(){
+    ios_base::sync_with_stdio(false);
+    
     int N, Q, x,caseCounter=0;
     while (cin >> N  >> Q && (N != 0 && Q != 0 )){
         caseCounter++;
         vector<int> marmores(N);
-        // inverto todos os numeros para negativo
-        // dessa forma o primeiro que eu achar sendo maior que x na verdade sera
-        // o primeiro menor que ele, dessa forma estando exatamente ao lado dele.
-        for (auto& m:marmores){cin >> m; (m=-m);}
+        for (auto& m:marmores){cin >> m;}
         sort(marmores.begin(), marmores.end());
         cout << "CASE# " << caseCounter << ":" << '\n';
         for (int i=0; i<Q; i++){
             cin >> x;
-            x = -x;
-            if (binary_search(marmores.begin(), marmores.end(), x)){
-                // retiro o valor achado de N pois esta organizado ao contrario
-                // somo +1 pois meu numero é logo apos esse que é o primeiro menor
-                cout << -x << " found at " << (N-(upper_bound(marmores.begin(), marmores.end(), x) - marmores.begin()))+1 << '\n';
+            auto it = lower_bound(marmores.begin(), marmores.end(), x);
+            
+            if (it == marmores.end() || *it != x){
+                //estourou e saiu do vetor
+                cout << x << " not found" << '\n';
             }else{
-                cout << -x << " not found" << '\n';
+                cout << x << " found at " << (it-marmores.begin()) +1<< '\n';
             }
         }
     }
